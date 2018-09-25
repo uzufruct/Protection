e121ef590d65e30a3b8d5970831a479cac8bf01d
Nikita.Sidorov <nickorussisch@gmail.com>
2018-09-25 20:28:03 +0700

    Encryption lib

---
+++ protection/cipher/__init__.py
@@ -0,0 +1,22 @@
+def shamir_encode(m, p):
+    return m
+
+
+def shamir_decode(msg, p):
+    return msg
+
+
+def elgamal_encode(m, p):
+    return m
+
+
+def elgamal_decode(msg, p):
+    return msg
+
+
+def rsa_encode(m):
+    return m
+
+
+def rsa_decode(msg):
+    return msg
